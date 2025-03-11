688970d35698b9e17685d4102f0e430d0d74d1b3
Евгений Яндутов <yandutka159@gmail.com>
2025-03-11 18:27:29 +0300

    Initial project commit

---
+++ src/Zoo.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Имплементируем наш зоопарк
+        }
+    }
+}
